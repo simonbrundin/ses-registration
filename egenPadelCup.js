4cad1856751bc7c2a3d8465422ef18ef2711c33f
Simon Brundin <simonbrundin@gmail.com>
2020-09-05 02:38:28 +0200

    inte klar än

--- egenPadelCup.js
+++ egenPadelCup.js
@@ -1,113 +1,33 @@
 let players = [
-  { id: 0, playedOpponents: [], playedWith: [] },
   { id: 1, playedOpponents: [], playedWith: [] },
   { id: 2, playedOpponents: [], playedWith: [] },
   { id: 3, playedOpponents: [], playedWith: [] },
   { id: 4, playedOpponents: [], playedWith: [] },
   { id: 5, playedOpponents: [], playedWith: [] },
   { id: 6, playedOpponents: [], playedWith: [] },
-  { id: 7, playedOpponents: [], playedWith: [] }
+  { id: 7, playedOpponents: [], playedWith: [] },
+  { id: 8, playedOpponents: [], playedWith: [] },
+  { id: 9, playedOpponents: [], playedWith: [] },
+  { id: 10, playedOpponents: [], playedWith: [] },
+  { id: 11, playedOpponents: [], playedWith: [] },
+  { id: 12, playedOpponents: [], playedWith: [] }
 ];
 
-let matches = [
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  },
-  {
-    hemma1: 0,
-    hemma2: 0,
-    borta1: 0,
-    borta2: 0
-  }
-];
-
+let matches = [];
+let potentialPlayers = [];
+const resetPotentialPlayers = () => {
+  players.forEach(player => {
+    potentialPlayers.push(player.id);
+  });
+};
+resetPotentialPlayers();
+console.log(potentialPlayers);
 let numberOfMatches = (players.length / 4) * (players.length - 1);
+console.log(numberOfMatches + " matcher");
 let rounds = players.length - 1;
-let matchesPerRound = numberOfMatches / rounds;
-
-let potentialPlayers = [0, 1, 2, 3, 4, 5, 6, 7];
+console.log(rounds + " omgångar");
 let currentPlayer = 0;
 let currentRound = 1;
-let matchInRound = 1;
-let hemma1 = 1000;
-let hemma2 = 1000;
-let borta1 = 1000;
-let borta2 = 1000;
 
 const nextPlayer = () => {
   if (currentPlayer === players.length - 1) {
@@ -116,13 +36,11 @@ const nextPlayer = () => {
     currentPlayer++;
   }
 };
-
 const checkPlayedWith = player => {
   while (players[currentPlayer].playedWith.includes(player)) {
     nextPlayer();
   }
 };
-
 const checkPlayedAgainst = player => {
   if (
     players[currentPlayer].playedOpponents.filter(player => player === player)
@@ -136,38 +54,41 @@ const checkPlayedAgainst = player => {
       checkPlayedWith(player);
   }
 };
+let hemma1 = 0;
+let hemma2 = 0;
+let borta1 = 0;
+let borta2 = 0;
 
 // Skapa matcher
 for (let index = 0; index < numberOfMatches; index++) {
   // hemma 1
-
   hemma1 = potentialPlayers[0];
-  matches[index].hemma1 = hemma1;
+  potentialPlayers.splice(potentialPlayers.indexOf(hemma1), 1);
 
   // hemma 2
-
-  nextPlayer();
-  checkPlayedWith(hemma1);
-  // checkPlayedAgainst(hemma1);
-
-  hemma2 = currentPlayer;
-  matches[index].hemma2 = hemma2;
+  hemma2 = potentialPlayers[0];
+  potentialPlayers.splice(potentialPlayers.indexOf(hemma2), 1);
 
   // borta 1
-  nextPlayer();
-  // checkPlayedAgainst(hemma1);
-  // checkPlayedAgainst(hemma2);
-  borta1 = currentPlayer;
-  matches[index].borta1 = borta1;
+  console.log(potentialPlayers[0]);
+  console.log(players[potentialPlayers[0]].playedOpponents);
+  console.log(
+    players[potentialPlayers[0]].playedOpponents.filter(
+      word => word === potentialPlayers[0]
+    )
+  );
+
+  borta1 = potentialPlayers[0];
+  potentialPlayers.splice(potentialPlayers.indexOf(borta1), 1);
 
   // borta 2
-  nextPlayer();
-  checkPlayedWith(borta1);
+  // nextPlayer();
+  // checkPlayedWith(borta1);
   // checkPlayedAgainst(hemma1);
   // checkPlayedAgainst(hemma2);
   // checkPlayedAgainst(borta1);
-  borta2 = currentPlayer;
-  matches[index].borta2 = borta2;
+  let borta2 = potentialPlayers[0];
+  potentialPlayers.splice(potentialPlayers.indexOf(borta2), 1);
 
   // Spara vilka alla spelat med och mot
   players[hemma1].playedWith.push(hemma2);
@@ -185,21 +106,26 @@ for (let index = 0; index < numberOfMatches; index++) {
 
   // Förbered för nästa match
 
-  console.log(hemma1 + "" + hemma2 + " " + borta1 + "" + borta2);
+  matches.push({
+    hemma1: hemma1,
+    hemma2: hemma2,
+    borta1: borta1,
+    borta2: borta2
+  });
 
-  if (matchInRound === matchesPerRound) {
-    matchInRound = 1;
-  } else {
-    potentialPlayers.splice(potentialPlayers.indexOf(hemma1), 1);
-    potentialPlayers.splice(potentialPlayers.indexOf(hemma2), 1);
-    potentialPlayers.splice(potentialPlayers.indexOf(borta1), 1);
-    potentialPlayers.splice(potentialPlayers.indexOf(borta2), 1);
-    console.log(potentialPlayers);
-    matchInRound++;
-  }
+  // console.log(hemma1 + "" + hemma2 + " " + borta1 + "" + borta2);
+  nextPlayer();
+  // if (currentRound === players.length) {
+  //   currentPlayer = 1;
+  // } else {
+  //   currentPlayer++;
+  // }
+  // for (let index = 0; index < array.length; index++) {
+  //   const element = array[index];
+  // }
 }
-
 console.log(matches);
+console.log(matches.length);
 // 1
 // 2
 
